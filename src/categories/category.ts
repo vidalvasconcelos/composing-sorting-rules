28f89fdda9e46147ff0039ea166d1b7308a36db2
Vidal Vasconcelos <vidalvasconcelos@icloud.com>
2025-10-01 22:30:41 +0200

    chore: simplify the application files

--- src/categories/category.ts
+++ src/categories/category.ts
@@ -1,4 +1,19 @@
-export type Category = {
+import * as Ord from "fp-ts/Ord";
+import {pipe} from "fp-ts/function";
+import * as s from "fp-ts/string";
+
+/**
+ * A Category interface
+ */
+export interface Category {
   readonly id: string;
   readonly name: string;
-};
+}
+
+/**
+ * The ordering of categories alphabetically by name
+ */
+export const ordCategoriesAlphabetically: Ord.Ord<Category> = pipe(
+  s.Ord,
+  Ord.contramap((coupon: Category): string => coupon.name),
+)
